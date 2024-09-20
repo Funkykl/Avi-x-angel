040056c0fccf2c79ca8491fb155238ced903bdff
Funkykl <foxtenebreux@gmail.com>
2024-09-20 08:22:18 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,19 +14,19 @@ global.socialm = "GitHub: avishka33" //ur github or insta name
 global.location = "Srilanka,southern, Galle" //ur location
 
 //new
-global.botname = '𝓐𝓿𝓲-𝔁-𝓪𝓷𝓰𝓮𝓵' //ur bot name
-global.ownernumber = '94772836332' //ur owner number
-global.ownername = '𝓡𝓮𝓪𝓵 𝓚𝓲𝓷𝓰 𝓐𝓿𝓲' //ur owner name
+global.botname = '|•OKLMBOY•|' //ur bot name
+global.ownernumber = '24102692931' //ur owner number
+global.ownername = '|•OKLMBOY•|' //ur owner name
 global.websitex = "https://youtube.com/@Avi_NetHunter?sub_confirmation=1"
 global.wagc = "https://whatsapp.com/channel/0029VaGisHDFHWq5jpuEmC2t"
 global.themeemoji = '🗡'
 global.wm = "𝘼𝙫𝙞-𝙞𝙣𝙛𝙞𝙣𝙖𝙩𝙚-𝙀𝙙𝙞𝙩𝙞𝙤𝙣"
-global.botscript = 'https://github.com/avishka33/Avi-x-angel' //script link
-global.packname = "𝓡𝓮𝓪𝓵 𝓚𝓲𝓷𝓰 𝓐𝓿𝓲"
-global.author = "𝓡𝓮𝓪𝓵 𝓚𝓲𝓷𝓰 𝓐𝓿𝓲\n\n+94772836332"
-global.creator = "94772836332@s.whatsapp.net"
-global.xprefix = '.'
-global.premium = ["94772836332","94787672517","94701074312"] // Premium User
+global.botscript = 'https://github.com/avishka33/***' //script link
+global.packname = "|•OKLMBOY•|"
+global.author = "FUCK•TOUT-LES_FDP_QUI_BZ_LES•MINEURS"
+global.creator = "24102692931@s.whatsapp.net"
+global.xprefix = '∆'
+global.premium = ["94772836332","94787672517","24102692931"] // Premium User
 global.hituet = 0
 
 //bot settings
